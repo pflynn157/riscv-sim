14a5c86cd3f1c99b4732a9bb4f2f4389416fac05
Patrick Flynn <pflynn157@gmail.com>
2022-10-06 10:45:35 -0400

    Added remaining instructions

--- cpu.cpp
+++ cpu.cpp
@@ -16,7 +16,11 @@ struct Data {
     uint8_t rs2 = 0;
     uint8_t func3 = 0;
     uint8_t func7 = 0;
-    uint32_t imm = 0;
+    uint32_t imm_i = 0;
+    uint32_t imm_b = 0;
+    uint32_t imm_s = 0;
+    uint32_t imm_u = 0;
+    uint32_t imm_j = 0;
 };
 
 //
@@ -67,9 +71,33 @@ struct CPU {
 Data *decode(uint32_t instr){
     Data *data = new Data;
     
+    data->opcode =  (uint8_t)(instr & 0b00000000000000000000000001111111);
+    data->rd =     (uint8_t)((instr & 0b00000000000000000000111110000000) >> 7);
+    data->imm_u = (uint32_t)((instr & 0b11111111111111111111000000000000) >> 12);
+    data->func3 =  (uint8_t)((instr & 0b00000000000000000111000000000000) >> 12);
+    data->rs1 =    (uint8_t)((instr & 0b00000000000011111000000000000000) >> 15);
+    data->imm_i = (uint32_t)((instr & 0b11111111111100000000000000000000) >> 20);
+    data->rs2 =    (uint8_t)((instr & 0b00000001111100000000000000000000) >> 20);
+    data->func7 =  (uint8_t)((instr & 0b11111110000000000000000000000000) >> 25);
+    
+    // Branch Imm: imm[4:1][11][10:5][12]
+    data->imm_b = ((uint32_t)(instr & 0x80000000) >> 19)
+        | ((instr & 0x80) << 4)
+        | ((instr >> 20) & 0x7e0)
+        | ((instr >> 7) & 0x1e);
+        
+    // Store Imm: imm[4:0] imm[11:5]
+    data->imm_s = data->rd | ((data->func7) >> 20);
+    
+    // J-Types
+    data->imm_j = ((uint32_t)(instr & 0x80000000) >> 11)
+        | (instr & 0xff000)              // imm[19:12]
+        | ((instr >> 9) & 0x800)         // imm[11]
+        | ((instr >> 20) & 0x7fe);       // imm[10:1]
+    
     // opcode
     // 6 - 0
-    data->opcode = (uint8_t)(instr & 0b00000000000000000000000000111111);
+    /*data->opcode = (uint8_t)(instr & 0b00000000000000000000000000111111);
     
     // rd
     // 11 - 7
@@ -81,11 +109,11 @@ Data *decode(uint32_t instr){
     
     // rs1
     // 19 - 15
-    data->rs1 = (uint8_t)((instr & 0b00000000000001111100000000000000) >> 15);
+    data->rs1 = (uint8_t)((instr & 0b000000000000011111000000000000000) >> 15);
     
     // rs2
     // 24 - 20
-    data->rs2 = (uint8_t)((instr & 0b0000000111110000000000000000000) >> 20);
+    data->rs2 = (uint8_t)((instr & 0b000000001111100000000000000000000) >> 20);
     
     // func7
     // 31 - 25
@@ -93,7 +121,7 @@ Data *decode(uint32_t instr){
     
     // imm
     // 31 - 20
-    data->imm = (uint32_t)((instr & 0b1111111111110000000000000000000) >> 20);
+    data->imm = (uint32_t)((instr & 0b1111111111110000000000000000000) >> 20);*/
     
     return data;
 }
@@ -104,7 +132,7 @@ Data *decode(uint32_t instr){
 void alu(Data *data, CPU *cpu, bool isImmediate) {
     uint32_t val1 = cpu->getRegister(data->rs1);
     uint32_t val2 = 0;
-    if (isImmediate) val2 = data->imm;
+    if (isImmediate) val2 = data->imm_i;
     else val2 = cpu->getRegister(data->rs2);
 
     // Add and sub (sub determined by func7)
@@ -235,7 +263,9 @@ void print_instruction(Data *data) {
     
     // First, print instruction type
     switch (data->opcode) {
+        //
         // R-Types
+        //
         case 0b0110011: {
             std::cout << "R-Type" << std::endl;
             printf("Func3: %X | Func7: %X\n", data->func3, data->func7);
@@ -259,15 +289,17 @@ void print_instruction(Data *data) {
                 
                 default: {}
             }
-            instr_str += " x" + to_string(data->rs1) + ", x" + to_string(data->rs2) + ", x" + to_string(data->rs2);
+            instr_str += " x" + to_string(data->rd) + ", x" + to_string(data->rs1) + ", x" + to_string(data->rs2);
             std::cout << instr_str << std::endl;
         } break;
         
+        //
         // I-Types
+        //
         case 0b0010011: {
             std::cout << "I-Type" << std::endl;
             printf("Func3: %X\n", data->func3);
-            std::cout << "Imm: " << data->imm << std::endl;
+            std::cout << "Imm: " << data->imm_i << std::endl;
             
             std::string instr_str = "";
             switch (data->func3) {
@@ -287,16 +319,31 @@ void print_instruction(Data *data) {
             }
             instr_str += " x" + to_string(data->rd) + ", x" + to_string(data->rs1) + ", ";
             if (data->func3 == 1 || data->func3 == 5) instr_str += to_string(data->rs2);
-            else instr_str += to_string(data->imm);
+            else instr_str += to_string(data->imm_i);
             std::cout << instr_str << std::endl;
         } break;
         
+        //
+        // JALR (I-TYPE)
+        //
+        case 0b1100111: {
+            std::cout << "I-Type (JALR)" << std::endl;
+            printf("Func3: %X\n", data->func3);
+            std::cout << "Imm: " << data->imm_i << std::endl;
+            
+            std::string instr_str = "jalr ";
+            instr_str += " x" + to_string(data->rd) + ", x" + to_string(data->rs1) + ", ";
+            instr_str += to_string(data->imm_i);
+            std::cout << instr_str << std::endl;
+        } break;
+        
+        //
         // B-Types
-        case 0x23:
-        case 0x2F: {
+        //
+        case 0b1100011: {
             std::cout << "B-Type" << std::endl;
             printf("Func3: %X\n", data->func3);
-            std::cout << "Imm: " << data->imm << std::endl;
+            std::cout << "Imm: " << (signed int)data->imm_b << std::endl;
             
             std::string instr_str = "";
             switch (data->func3) {
@@ -309,7 +356,78 @@ void print_instruction(Data *data) {
                 
                 default: {}
             }
-            instr_str += " x" + to_string(data->rd) + ", x" + to_string(data->rs1) + ", SOMEWHERE";
+            instr_str += " x" + to_string(data->rs1) + ", x" + to_string(data->rs2) + ", SOMEWHERE";
+            std::cout << instr_str << std::endl;
+        } break;
+        
+        //
+        // Loads
+        //
+        case 0b0000011: {
+            std::cout << "I-Type (Loads)" << std::endl;
+            printf("Func3: %X\n", data->func3);
+            std::cout << "Offset: " << data->imm_i << std::endl;
+            
+            std::string instr_str = "lw x" + to_string(data->rd) + ", ";
+            instr_str += to_string(data->imm_i) + "(x";
+            instr_str += to_string(data->rs1) + ")";
+            std::cout << instr_str << std::endl;
+        } break;
+        
+        //
+        // Stores
+        // S-Type
+        //
+        case 0b0100011: {
+            uint32_t imm = data->imm_s;
+        
+            std::cout << "S-Type (Store)" << std::endl;
+            printf("Func3: %X\n", data->func3);
+            std::cout << "Offset: " << imm << std::endl;
+            
+            std::string instr_str = "sw x" + to_string(data->rs2) + ", ";
+            instr_str += to_string(imm) + "(x";
+            instr_str += to_string(data->rs1) + ")";
+            std::cout << instr_str << std::endl;
+        } break;
+        
+        //
+        // LUI
+        // U-Type
+        //
+        case 0b0110111: {
+            std::cout << "U-Type (LUI)" << std::endl;
+            std::cout << "Offset: " << data->imm_u << std::endl;
+            
+            std::string instr_str = "lui x" + to_string(data->rd) + ", ";
+            instr_str += to_string(data->imm_u);
+            std::cout << instr_str << std::endl;
+        } break;
+        
+        //
+        // AUIPC
+        // U-Type
+        //
+        case 0b0010111: {
+            std::cout << "U-Type (AUIPC)" << std::endl;
+            std::cout << "Offset: " << data->imm_u << std::endl;
+            
+            std::string instr_str = "auipc x" + to_string(data->rd) + ", ";
+            instr_str += to_string(data->imm_u);
+            std::cout << instr_str << std::endl;
+        } break;
+        
+        //
+        // JAL | J-Type
+        //
+        case 0b1101111: {
+            int32_t imm = (int32_t)data->imm_j;
+        
+            std::cout << "J-Type (JAL)" << std::endl;
+            std::cout << "Offset: " << imm << std::endl;
+            
+            std::string instr_str = "jal x" + to_string(data->rd) + ", ";
+            instr_str += to_string(imm);
             std::cout << instr_str << std::endl;
         } break;
         
@@ -332,48 +450,70 @@ int main() {
     // LE: 0000 0000 1010 0000 0000 0001 0001 0011
     uint32_t instr2 = 0x00A00113;
     
-    int instr_count = 26;
-    uint32_t instr_memory[] = {
-        0x00a00093,
-        0x01400113,
-        0x00a12193,
-        0x00a13213,
-        0x00a14293,
-        0x00a16313,
-        0x00a17393,
-        0x00a11413,
-        0x00a15493,
-        0x40a15513,
-        0x002081b3,
-        0x40208233,
-        0x002092b3,
-        0x0020d333,
-        0x4020d3b3,
-        0x0020f433,
-        0x0020e4b3,
-        0x0020c533,
-        0x0020a5b3,
-        0x0020b633,
-        0x00209463,
-        0xfadff06f,
-        0x00208463,
-        0xfa5ff06f,
-        0x0020d463,
-        0xf9dff06f,
-        0x0020c463,
-        0xf95ff06f,
-        0x0020f463,
-        0xf8dff06f,
-        0x0020e463,
-        0xf85ff06f
-    };
+    std::vector<uint32_t> instr_memory;
+    
+    instr_memory.push_back(0x00a00093);     // addi x1, x0, 10
+    instr_memory.push_back(0x01400113);     // addi x2, x0, 20
+    instr_memory.push_back(0x00a12193);     // slti x3, x2, 10
+    instr_memory.push_back(0x00a13213);     // sltiu x4, x2, 10
+    instr_memory.push_back(0x00a14293);     // xori x5, x2, 10
+    instr_memory.push_back(0x00a16313);     // ori x6, x2, 10
+    instr_memory.push_back(0x00a17393);     // andi x7, x2, 10
+    instr_memory.push_back(0x00a11413);     // slli x8, x2, 10
+    instr_memory.push_back(0x00a15493);     // srli x9, x2, 10
+    instr_memory.push_back(0x40a15513);     // srai x10, x2, 10
+    
+    instr_memory.push_back(0x002081b3);     // add x3, x1, x2
+    instr_memory.push_back(0x40208233);     // sub x4, x1, x2
+    instr_memory.push_back(0x002092b3);     // sll x5, x1, x2
+    instr_memory.push_back(0x0020d333);     // srl x6, x1, x2
+    instr_memory.push_back(0x4020d3b3);     // sra x7, x1, x2
+    instr_memory.push_back(0x0020f433);     // and x8, x1, x2
+    instr_memory.push_back(0x0020e4b3);     // or x9, x1, x2
+    instr_memory.push_back(0x0020c533);     // xor x10, x1, x2
+    instr_memory.push_back(0x0020a5b3);     // slt x11, x1, x2
+    instr_memory.push_back(0x0020b633);     //sltu x12, x1, x2
+    
+    instr_memory.push_back(0x00208063);     // beq x1, x2, LOOP
+    instr_memory.push_back(0xfe209ee3);     // bne x1, x2, LOOP
+    instr_memory.push_back(0xfe20cce3);     // blt x1, x2, LOOP
+    instr_memory.push_back(0xfe20dae3);     // bge x1, x2, LOOP
+    instr_memory.push_back(0xfe20e8e3);     // bltu x1, x2, LOOP
+    instr_memory.push_back(0xfe20f6e3);     // bgeu x1, x2, LOOP
+    
+    instr_memory.push_back(0x00012083);     // lw x1, 0(x2)
+    instr_memory.push_back(0x00a1a103);     // lw x2, 10(x3)
+    instr_memory.push_back(0x01422183);     // lw x3, 20(x4)
+    instr_memory.push_back(0x00e7a203);     // lw x4, 14(x15)
+    instr_memory.push_back(0x00f0a503);     // lw x10, 15(x1)
+    
+    instr_memory.push_back(0x00ab2023);     // sw x10, 0(x22)
+    instr_memory.push_back(0x014f2523);     // sw x20, 10(x30)
+    instr_memory.push_back(0x00d22a23);     // sw x13, 20(x4)
+    instr_memory.push_back(0x00e7a723);     // sw x14, 14(x15)
+    instr_memory.push_back(0x0010a7a3);     // sw x1, 15(x1)
+    
+    instr_memory.push_back(0x007e40b7);     // lui x1, 2020
+    instr_memory.push_back(0x007e5137);     // lui x2, 2021
+    instr_memory.push_back(0x007e61b7);     // lui x3, 2022
+    
+    instr_memory.push_back(0x000bc617);     // auipc x12, 188
+    instr_memory.push_back(0x0012bb17);     // auipc x22, 299
+    instr_memory.push_back(0x0018f797);     // auipc x15, 399
+    
+    instr_memory.push_back(0x0080056f);     // jal x10, BOOT1
+    instr_memory.push_back(0xffdff5ef);     // jal x11, BOOT2
+    
+    instr_memory.push_back(0x065500e7);     // jalr x1, x10, 101
+    instr_memory.push_back(0x0c960167);     // jalr x2, x12, 201
+    
     
     //Data *d1 = decode(instr1);
     //print_instruction(d1);
    // 
     //Data *d2 = decode(instr2);
     //print_instruction(d2);
-    for (int i = 0; i<instr_count; i++) {
+    for (int i = 0; i<instr_memory.size(); i++) {
         Data *d = decode(instr_memory[i]);
         print_instruction(d);
     }
--- input.asm
+++ input.asm
@@ -1,5 +1,5 @@
 addi x1, x0, 10
-addi x2, x0 20
+addi x2, x0, 20
 slti x3, x2, 10
 sltiu x4, x2, 10
 xori x5, x2, 10
@@ -26,3 +26,30 @@ blt x1, x2, LOOP
 bge x1, x2, LOOP
 bltu x1, x2, LOOP
 bgeu x1, x2, LOOP
+
+lw x1, 0(x2)
+lw x2, 10(x3)
+lw x3, 20(x4)
+lw x4, 14(x15)
+lw x10, 15(x1)
+
+sw x10, 0(x22)
+sw x20, 10(x30)
+sw x13, 20(x4)
+sw x14, 14(x15)
+sw x1, 15(x1)
+
+lui x1, 2020
+lui x2, 2021
+lui x3, 2022
+
+auipc x12, 188
+auipc x22, 299
+auipc x15, 399
+
+jal x10, BOOT1
+jal x11, BOOT2
+
+jalr x1, x10, 101
+jalr x2, x12, 201
+
