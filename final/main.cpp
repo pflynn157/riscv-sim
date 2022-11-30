e807e56bbb43fdd29fc6931d163085dbbeff70b1
Patrick Flynn <pflynn157@gmail.com>
2022-11-30 15:39:21 -0500

    Print setup

--- final/main.cpp
+++ final/main.cpp
@@ -13,6 +13,26 @@ int main(int argc, char **argv) {
     c2->setBus(bus);
     c3->setBus(bus);
     c4->setBus(bus);
+    std::cout << "=======================================" << std::endl;
+    
+    // Test 1
+    // Read: Read -> Invalid -> Exclusive
+    // Host: Cache 1
+    
+    
+    // Final print
+    std::cout << "=C1=========================" << std::endl;
+    c1->print();
+    std::cout << std::endl;
+    std::cout << "=C2=========================" << std::endl;
+    c2->print();
+    std::cout << std::endl;
+    std::cout << "=C3=========================" << std::endl;
+    c3->print();
+    std::cout << std::endl;
+    std::cout << "=C4=========================" << std::endl;
+    c4->print();
+    std::cout << std::endl;
 
     return 0;
 }
